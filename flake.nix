09bf2494ec298722feab1434fee83619f83c8aef
Andromeda <stroev.andrew@gmail.com>
2025-08-03 04:59:14 -0700

    Update flake description

--- flake.nix
+++ flake.nix
@@ -1,18 +1,21 @@
 {
-  description = "puritytest";
+  description = "Purity Test Maker";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
-  outputs = inputs: let
-    system = "x86_64-linux";
-    pkgs = inputs.nixpkgs.legacyPackages.${system};
-  in {
-    devShells.${system}.default = pkgs.mkShell {
-      packages = with pkgs; [
-        bun
-      ];
+  outputs =
+    inputs:
+    let
+      system = "x86_64-linux";
+      pkgs = inputs.nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [
+          bun
+        ];
+      };
     };
-  };
 }
