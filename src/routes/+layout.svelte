fba17164e186f0e3f23a458a5b4480e79944ec5c
Andromeda <stroev.andrew@gmail.com>
2025-08-03 05:37:59 -0700

    Add favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,15 @@
 <script lang="ts">
     import '../app.css';
-    import favicon from '$lib/assets/favicon.svg';
     import { page } from '$app/state';
 
     let { children } = $props();
 </script>
 
 <svelte:head>
-    <link rel="icon" href={favicon} />
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+    <link rel="manifest" href="/site.webmanifest" />
 
     <meta property="og:site_name" content="The Official Purity Test Maker" />
     <meta property="og:type" content="website" />
