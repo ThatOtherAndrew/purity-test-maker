c7434754d56af7832e470a36ef8132552b832ae3
Andromeda <stroev.andrew@gmail.com>
2025-08-02 22:22:27 -0700

    Add flavour text to purity test

--- src/lib/components/PurityTest.svelte
+++ src/lib/components/PurityTest.svelte
@@ -1,7 +1,29 @@
 <script lang="ts">
     import PurityTestHeader from './PurityTestHeader.svelte';
 
-    let { name } = $props();
+    let { name, introParagraph = null, completionConsequence = 'death' } = $props();
 </script>
 
 <PurityTestHeader {name} />
+
+{#if introParagraph !== null}
+    <p class="pre-wrap">
+        <i>{introParagraph}</i>
+    </p>
+{/if}
+
+<p>
+    <b>
+        Caution: This is not a bucket list. Completion of all items on this test will likely result
+        in {completionConsequence}.
+    </b>
+</p>
+
+<br />
+<p>Click on every item you have done.</p>
+
+<style>
+    .pre-wrap {
+        white-space: pre-line;
+    }
+</style>
--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -1,6 +1,12 @@
 <script lang="ts">
-	import PurityTest from '$lib/components/PurityTest.svelte';
-	import { page } from '$app/state';
+    import PurityTest from '$lib/components/PurityTest.svelte';
+    import { page } from '$app/state';
+
+    const slug = page?.params?.slug;
+    const introParagraph = `
+        The Purity Test has historically served as a segue from O-week to true college life at ${slug}.
+        It's a voluntary opportunity for O-week groups to bond, and for students to track the maturation of their experiences throughout college.
+    `.trim();
 </script>
 
-<PurityTest name={page?.params?.slug} />
+<PurityTest name={slug} {introParagraph} />
