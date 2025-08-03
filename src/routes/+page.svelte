27758f8bbd92e8f0dcecfcd369b435cc519233cc
Andromeda <stroev.andrew@gmail.com>
2025-08-03 04:03:38 -0700

    Create home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,46 @@
-<h1>Make your own Purity Test</h1>
+<script>
+    import PurityTest from '$lib/components/PurityTest.svelte';
+    import PurityTestHeader from '$lib/components/PurityTestHeader.svelte';
+    import purityTests from '$lib/purity-tests';
+</script>
+
+<PurityTest noHeader>
+    <PurityTestHeader
+        name="Purity Test Maker"
+        isFullTitle
+        stamp="The Official"
+        comment="{Object.keys(purityTests).length} Purity Tests and counting..."
+    />
+
+    <h2>Choose a test:</h2>
+    {#each Object.entries(purityTests) as [testId, test] (testId)}
+        <a href={testId}>
+            <div>
+                <h3>{test.name} Purity Test</h3>
+                <i>{test.description}</i>
+            </div>
+        </a>
+    {/each}
+</PurityTest>
+
+<style>
+    a {
+        text-decoration: none;
+    }
+
+    div {
+        justify-content: center;
+        padding: 1em;
+        margin: 10px;
+        font-size: 1em;
+        text-align: center;
+        border: solid;
+        border-radius: 20px;
+        background-color: rgba(102, 255, 220, 0.3);
+        color: rgb(0, 0, 5);
+    }
+
+    h3 {
+        margin-top: 0;
+    }
+</style>
