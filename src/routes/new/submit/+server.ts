170aec604fef349cdbf80a3621a822a2b9ebb964
Andromeda <stroev.andrew@gmail.com>
2025-08-07 22:28:13 -0700

    Fix missing await

--- src/routes/new/submit/+server.ts
+++ src/routes/new/submit/+server.ts
@@ -2,6 +2,6 @@ import { type RequestHandler } from '@sveltejs/kit';
 import { savePurityTest } from '$lib/utils';
 
 export const POST: RequestHandler = async ({ request }) => {
-    const testId = savePurityTest(await request.json());
+    const testId = await savePurityTest(await request.json());
     return new Response(JSON.stringify({ testId }));
 };
