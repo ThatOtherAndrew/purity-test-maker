f9376f0b17fd1be52df5b2a4de8bff775908dd33
Andromeda <stroev.andrew@gmail.com>
2025-08-09 03:23:26 -0400

    Missing await 🥀

--- src/routes/new/+page.server.ts
+++ src/routes/new/+page.server.ts
@@ -2,5 +2,5 @@ import { countPurityTests } from '$lib/utils';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async () => {
-    return { testCount: countPurityTests() };
+    return { testCount: await countPurityTests() };
 };
